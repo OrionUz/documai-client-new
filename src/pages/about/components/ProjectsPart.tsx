b4c85551245bcd69b7549ba744c0ca5cb216afce
Asror07 <nematovasror07@gmail.com>
2023-06-29 07:44:58 -0400

    one changed

--- src/pages/about/components/ProjectsPart.tsx
+++ src/pages/about/components/ProjectsPart.tsx
@@ -2,7 +2,7 @@ import { ProjectsPartProps } from "../type";
 
 function ProjectsPart({ item }: ProjectsPartProps) {
   return (
-    <a href={item.link} className="about-buttom-parts-part" target="_blank" rel="no referrer">
+    <a href={item.link} className="about-buttom-parts-part" target="_blank" rel="noreferrer">
       <div className="about-buttom-parts-part-box">
         <div className="about-buttom-parts-part-box-title">{item.title}</div>
         <div className="about-buttom-parts-part-box-text">{item.text}</div>
