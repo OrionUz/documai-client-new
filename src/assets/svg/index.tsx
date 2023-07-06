ecede5e1d352c146b7892fcbddb8b298095d3a61
Avazbek2906 <avazbeksotvoldiyev2906@gmail.com>
2023-07-06 14:54:06 +0500

    add delete project

--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -1,6 +1,3 @@
-import { ButtonProps } from "antd";
-import { IconType } from "antd/es/notification/interface";
-
 export const CorrectActiveSvg = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle cx="12" cy="12" r="12" fill="#3EA972" />
