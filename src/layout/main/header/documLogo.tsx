79086af41f5b27a9d4b7dacc5f473f13b3b8bdc9
Asror07 <nematovasror07@gmail.com>
2024-04-15 00:28:36 +0500

    blue journey logo fixed

--- src/layout/main/header/documLogo.tsx
+++ src/layout/main/header/documLogo.tsx
@@ -5,7 +5,7 @@ function Logo() {
   return (
     <Link to="/">
       <img
-        className={`logo ${isDocum ? "" : "-blue"}`}
+        className={`logo${isDocum ? "" : "-blue"}`}
         src={require(`../../../assets/img/logo${isDocum ? "" : "2"}.png`)}
         alt="logo img error"
       />
